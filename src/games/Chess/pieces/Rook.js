b03e24dbe16dcbf2ab601cda0ffec3a06bf35cf6
StavoS <StavSinai@gmail.com>
2024-07-28 20:52:12 +0300

    finish rook

--- src/games/Chess/pieces/Rook.js
+++ src/games/Chess/pieces/Rook.js
@@ -4,10 +4,46 @@ class Rook extends Piece {
     constructor(x, y, color) {
         super(x, y, color);
         this._pieceName = 'Rook';
-        this.updatePieceImg();
+        this._updatePieceImg();
     }
 
-    calcPossibleMoves(chessBoard) {}
+    _calc(chessBoard, enemyColor) {
+        let possibleMoves = [];
+        const boardSize = chessBoard.length;
+        const { x: startX, y: startY } = this.position;
+
+        const directions = [
+            { dx: 1, dy: 0 },
+            { dx: -1, dy: 0 },
+            { dx: 0, dy: 1 },
+            { dx: 0, dy: -1 },
+        ];
+
+        for (const { dx, dy } of directions) {
+            let x = startX;
+            let y = startY;
+
+            while (true) {
+                x += dx;
+                y += dy;
+
+                if (x >= boardSize || x < 0 || y >= boardSize || y < 0) break;
+
+                let cell = chessBoard[x][y];
+                if (cell) {
+                    if (cell.color === this._color) break;
+
+                    if (cell.color === enemyColor) {
+                        possibleMoves.push({ x, y });
+                        break;
+                    }
+                }
+                possibleMoves.push({ x, y });
+            }
+        }
+
+        return possibleMoves;
+    }
 }
 
 export default Rook;
